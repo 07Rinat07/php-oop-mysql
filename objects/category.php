109c576a887cc985967fbe26448ad12b443c7f5b
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-05 14:50:39 +0500

    Create method readName()

--- objects/category.php
+++ objects/category.php
@@ -31,4 +31,19 @@ class Category
 
         return $stmt;
     }
+
+    // получение названия категории по её ID
+    function readName()
+    {
+        // запрос MySQL
+        $query = "SELECT name FROM " . $this->table_name . " WHERE id = ? limit 0,1";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->name = $row["name"];
+    }
 }
