df8fd0a8a467d4ab3515fb4e9f147b8f18b9b126
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-05 18:24:25 +0500

    Create button Show All products

--- update_product.php
+++ update_product.php
@@ -8,7 +8,11 @@ $page_title = "Обновление товара";
 include_once "layout_header.php";
 ?>
 
-    <!-- здесь будет контент -->
+    <div class="right-button-margin">
+        <a href="index.php" class="btn btn-default pull-right">Просмотр всех товаров</a>
+    </div>
+
+    <!-- здесь будет форма обновления товара -->
 
 <?php // подвал
 require_once "layout_footer.php";
