6b391e3785cab86e5443c2ca2da6ee74c79019a0
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-05 17:21:38 +0500

    BugFix show category list

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
 class Database
 {
     // укажите свои собственные учетные данные для базы данных
-    private $host = "localhost";
-    private $db_name = "php_oop";
+    private $host = "127.0.0.1";
+    private $db_name = "php-oop";
     private $username = "root";
-    private $password = "root";
+    private $password = "";
     public $conn;
 
     // получение соединения с базой данных
@@ -16,6 +16,7 @@ class Database
 
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8mb4");
         } catch (PDOException $exception) {
             echo "Ошибка соединения: " . $exception->getMessage();
         }
