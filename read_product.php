0275ccd5d0fbc58d96df07043ea162ac01342b70
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-05 19:06:09 +0500

    Create product page

--- read_product.php
+++ read_product.php
@@ -1,1 +1,17 @@
 <?php
+
+// установка заголовка страницы
+$page_title = "Страница товара (чтение одного товара)";
+
+require_once "layout_header.php";
+?>
+
+    <!-- ссылка на все товары -->
+    <div class="right-button-margin">
+        <a href="index.php" class="btn btn-primary pull-right">
+            <span class="glyphicon glyphicon-list"></span> Просмотр всех товаров
+        </a>
+    </div>
+
+<?php // подвал
+require_once "layout_footer.php";
