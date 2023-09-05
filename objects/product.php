7f3bb508b6d36c747307599e304b40e0e77eb333
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-05 21:11:17 +0500

    Method for delete product in class Product

--- objects/product.php
+++ objects/product.php
@@ -146,5 +146,20 @@ class Product
 
         return false;
     }
+    // метод для удаления товара
+    function delete()
+    {
+        // запрос MySQL для удаления
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+
+        if ($result = $stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
